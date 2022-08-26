0e3b7a4338b10b8b5d6735224500efcbc62f0376
amir <torabi46@gmail.com>
2022-08-26 12:06:48 +0430

    Initialize app

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+  <div class="text-center">
+    <h1 class="text-metal bg-white py-1.5 font-bold text-3xl">
+      Welcome to page
+    </h1>
+  </div>
+</template>
